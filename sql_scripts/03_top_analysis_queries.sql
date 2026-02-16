f0f3c01a0f52cd56fbcf34df4bd0251f3c07ac6e
Shourya Sarraf <shourya.sarraf933@gmail.com>
2026-02-16 10:46:11 +0530

    Add analysis queries for FY 2021 net sales
    Added SQL queries to analyze top markets, customers, and products for FY 2021.

--- sql_scripts/03_top_analysis_queries.sql
+++ sql_scripts/03_top_analysis_queries.sql
@@ -1,1 +1,51 @@
--- coming
+/*
+01 -- Business Question:
+Which markets generate the highest net sales in FY 2021?
+
+Why it matters:
+Helps leadership prioritize geographic investments.
+*/
+SELECT
+    market,
+    ROUND(SUM(net_sales) / 1000000, 2) AS net_sales_mln
+FROM net_sales_view
+WHERE fiscal_year = 2021
+GROUP BY market
+ORDER BY net_sales_mln DESC
+LIMIT 5;
+---
+/*
+02 -- Business Question:
+Who are the top revenue-contributing customers?
+
+Why it matters:
+Supports account management and contract prioritization.
+*/
+
+SELECT
+    customer,
+    ROUND(SUM(net_sales) / 1000000, 2) AS net_sales_mln
+FROM net_sales_view
+WHERE fiscal_year = 2021
+GROUP BY customer
+ORDER BY net_sales_mln DESC
+LIMIT 5;
+--- 
+/*
+03 -- Business Question:
+Which products drive the most revenue?
+
+Why it matters:
+Informs product strategy and inventory planning.
+*/
+SELECT
+    product,
+    ROUND(SUM(net_sales) / 1000000, 2) AS net_sales_mln
+FROM net_sales_view
+WHERE fiscal_year = 2021
+GROUP BY product
+ORDER BY net_sales_mln DESC
+LIMIT 5;
+---
+
+
